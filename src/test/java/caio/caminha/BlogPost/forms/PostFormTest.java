3e9b6bd4b811c96e2f6016e30782a61dc31cea40
caio caminha <caminhacaiopro@gmail.com>
2021-05-27 15:54:36 -0300

    Implementado os testes do PostForm

---
+++ src/test/java/caio/caminha/BlogPost/forms/PostFormTest.java
@@ -0,0 +1,48 @@
+package caio.caminha.BlogPost.forms;
+
+import caio.caminha.BlogPost.models.Post;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+
+public class PostFormTest {
+
+    @Test
+    public void converteParaPostTest(){
+        PostForm form = PostForm
+                .builder()
+                .titulo("Estudar Spring Boot")
+                .texto("Estudar todo o framework Spring")
+                .data(LocalDate.now())
+                .build();
+
+        Post post = new Post("Estudar", "Estudar Java", LocalDate.now());
+        Post updatedPost = form.updateParaPost(post);
+        Assertions.assertThat(updatedPost).isNotNull();
+        Assertions.assertThat(updatedPost.getTitulo()).isEqualTo(form.getTitulo());
+        Assertions.assertThat(updatedPost.getTexto()).isEqualTo(form.getTexto());
+        Assertions.assertThat(updatedPost.getData()).isEqualTo(form.getData());
+    }
+
+    @Test
+    public void updateParaPostTest(){
+        PostForm form = PostForm
+                .builder()
+                .titulo("Estudar Spring Boot")
+                .texto("Estudar todo o framework Spring")
+                .data(LocalDate.now())
+                .build();
+
+
+        Post updatedPost = form.converteParaPost();
+        Assertions.assertThat(updatedPost).isNotNull();
+        Assertions.assertThat(updatedPost.getTitulo()).isEqualTo(form.getTitulo());
+        Assertions.assertThat(updatedPost.getTexto()).isEqualTo(form.getTexto());
+        Assertions.assertThat(updatedPost.getData()).isEqualTo(form.getData());
+    }
+}
--- src/test/java/caio/caminha/BlogPost/services/PostServiceTest.java
+++ src/test/java/caio/caminha/BlogPost/services/PostServiceTest.java
@@ -1,27 +1,17 @@
 package caio.caminha.BlogPost.services;
 
-import caio.caminha.BlogPost.dto.PostDto;
 import caio.caminha.BlogPost.forms.PostForm;
 import caio.caminha.BlogPost.models.Post;
-import org.apache.catalina.LifecycleState;
 import org.assertj.core.api.Assertions;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
